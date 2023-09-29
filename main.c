366a9be43c7855660cf6e34812d4880b972263fd
l-kaushhik <contact.lokeshkaushik@gmail.com>
2023-09-29 12:04:01 +0530

    intoPostfix function code complete

--- main.c
+++ main.c
@@ -8,8 +8,26 @@ typedef struct {
 
 char * intoPostfix(char *exp){
 	stack sp;
-	
+	char *postfix = (char *)malloc(strlen(exp)*sizeof(char));
+	int i = 0, j = 0;
 
+	while(exp[i] != '\0'){
+		if(!isOperator(exp[i])){
+			postfix[j] = exp[i];
+			i++;
+			j++;
+		}
+		else{
+			if(precedence(exp[i] > precedence(sp.top))){
+				push(sp,exp[i]);
+			}
+			else{
+				postfix[j] = pop(sp);
+				j++;
+			}
+		}
+	}
+	return postfix;
 }
 
 int main()
