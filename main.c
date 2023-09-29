6b4741eb708fbc25c9b8806bd059bc31a108a0c4
l-kaushhik <contact.lokeshkaushik@gmail.com>
2023-09-29 12:17:51 +0530

    Added stack related functions

--- main.c
+++ main.c
@@ -3,8 +3,47 @@
 
 typedef struct {
 	char top;
+	int size;
 	char *arr;
 }stack;
+
+int isEmpty(stack s)
+{
+    if (s.top == -1)
+        return 1;
+    else
+        return 0;
+}
+
+void push(stack s, char value)
+{
+    if (s.top == s.size-1)
+    {
+        int newSize = s.size + 5;
+        char *newArr = (char *)realloc(s.arr, sizeof(char) * newSize);
+        if(newArr == NULL){
+			printf("Memory can't be allocated\n");
+			exit(EXIT_FAILURE);
+		}
+        s.arr = newArr;
+        s.size = newSize;
+    }
+
+    s.top++;
+	s.arr[s.top] = value;
+}
+
+int pop(stack s)
+{
+    if( isEmpty(s) == 1)
+    {
+        printf("Stack is empty\n");
+        return -1;
+    }
+	int temp = s.top;
+    s.top--;
+	return temp;
+}
 
 int precedence(char exp){
 	if (exp == '+' || exp == '-')
@@ -17,8 +56,10 @@ int precedence(char exp){
 		return -1;
 }
 
+
 char * intoPostfix(char *exp){
 	stack sp;
+	sp.size = 0;
 	char *postfix = (char *)malloc(strlen(exp)*sizeof(char));
 	int i = 0, j = 0;
 
@@ -48,7 +89,7 @@ char * intoPostfix(char *exp){
 int main()
 {
 	char exp[] = {"a-b*d+c"};
-	
-	intoPostfix(exp);
+
+	printf("%s",intoPostfix(exp));
     return 0;
 }
